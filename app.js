55954127703283b73d699c9bc0044e0e619f6f03
Samuel Guimarães <sguimaraes@avenuecode.com>
2018-03-19 19:19:14 -0300

    Setting up folder to create node application

--- app.js
+++ app.js
@@ -22,17 +22,17 @@ class InvalidArgumentException extends Error {
 function widthdrawMoney(value = 0){
 	const money = [100,50,20,10];
   let sets = [];
-    try { 
+    try {
         if( value === 0 || value === null ) return sets;
-        
+
         if( value < 0 ) throw new InvalidArgumentException(ERROR_TYPES.NEGATIVE);
-        
+
         if(isNaN(value)) throw new InvalidArgumentException(ERROR_TYPES.NAN);
-        
+
         if(value % money[money.length-1] !== 0) throw new NoteUnavailableException(ERROR_TYPES.NOT_AVAILABLE);
-        
+
         if(!Number.isInteger(value)) throw new NoteUnavailableException(ERROR_TYPES.CENTS);
-        
+
         for(let i = 0; value > 0; i++) {
           let r = 0;
         	if( money[i] <= value ){ // First check to ignore notes that are bigger than amount requested
@@ -41,7 +41,7 @@ function widthdrawMoney(value = 0){
             sets.push(money[i]); // Store banknotes to withdraw
           }
         }
-        
+
         return sets;
     }
     catch(err) {
